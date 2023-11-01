332c495a77a5fab19913649b51e653cfad515536
HadeerAdelouf <hadiradel999@gmail.com>
2023-10-31 18:18:27 -0700

    7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    lower = ord(c)
-
-    if lower in range(97, 123):
+    number = ord(c)
+    if number in range(97, 123):
         return (True)
     else:
         return (False)
