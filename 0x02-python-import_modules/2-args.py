bac5fdd5e805cc85e7bc4b52d97c6e444e499999
HadeerAdelouf <hadiradel999@gmail.com>
2023-11-02 17:39:52 -0700

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,5 +9,10 @@ if __name__ == "__main__":
         print("{} argument:".format(i))
     else:
         print("{} arguments:".format(i))
-    for x in range(i):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
