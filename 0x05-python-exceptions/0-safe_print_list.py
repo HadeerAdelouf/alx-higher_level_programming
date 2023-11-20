34270ca898c9d37b594007faff5e16f38bb493c4
HadeerAdelouf <hadiradel999@gmail.com>
2023-11-20 14:53:07 -0800

    0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    indexx = 0
+    try:
+        while indexx is not x:
+            print(my_list[indexx], end='')
+            indexx += 1
+    except IndexError:
+        break
+    print()
+    return indexx
