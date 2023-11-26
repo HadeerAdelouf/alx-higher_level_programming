55f60518aa2da3ddbfdea08cb77b8719add4b105
HadeerAdelouf <hadiradel999@gmail.com>
2023-11-26 05:58:02 +0200

    4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module for print_square method"""
+
+def print_square(size):
+        """ print square function
+        Arguments:
+            @size: size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        for i in range(size):
+            for j in range(size):
+                print('#', end='')
+            print()
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
