9d858cd22e0b0197b73e1f63e7730c2482415e89
HadeerAdelouf <hadiradel999@gmail.com>
2023-12-06 22:54:49 +0200

    1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a file writing function"""
+
+
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file.
+
+    Args:
+        filename: The name of the file to write.
+        text: The text to write to the file.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
