d3e5b85c77fcd61f78da3860de27fd34fe63e443
HadeerAdelouf <hadiradel999@gmail.com>
2023-12-10 01:02:20 +0200

    10

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -87,3 +87,23 @@ class Rectangle(Base):
         """Return the str representation of the Rect"""
         return "[Rectangle] ({}) {}/{} - {}/{}".\
             format(self.id,self.x, self.y,self.width, self.height)
+    
+    def __update(self, id=None, width=None, height=None, x=None, y=None):
+        '''method that updates instance attributes via *args.'''
+        if id is not None:
+            self.id = id
+        if width is not None:
+            self.width = width
+        if height is not None:
+            self.height = height
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        '''Updates instance attributes via no-keyword & keyword args.'''
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
