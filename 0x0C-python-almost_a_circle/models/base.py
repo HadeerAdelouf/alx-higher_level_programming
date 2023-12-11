a2b28fa5a3b52dbc7367bccfe1158d14fc9297da
HadeerAdelouf <hadiradel999@gmail.com>
2023-12-11 05:09:49 +0200

    100

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ Module that contains class Base """
 import json
+import csv
+import os.path
+
 
 class Base:
     """ Class Base """
@@ -45,3 +48,70 @@ class Base:
         if not json_string:
             return []
         return json.loads(json_string)
+    
+    @classmethod
+    def create(cls, **dictionary):
+        """ Create an instance """
+        if cls.__name__ == "Rectangle":
+            new = cls(10, 10)
+        else:
+            new = cls(10)
+        new.update(**dictionary)
+        return new
+    
+    @classmethod
+    def load_from_file(cls):
+        """ Returns a list of instances """
+        filename = "{}.json".format(cls.__name__)
+
+        if os.path.exists(filename) is False:
+            return []
+
+        with open(filename, 'r') as f:
+            list_str = f.read()
+
+        LIST_d = cls.from_json_string(list_str)
+        list_Ins = []
+
+        for index in range(len(LIST_d)):
+            list_Ins.append(cls.create(**LIST_d[index]))
+
+        return list_Ins
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        '''Saves object to csv file'''
+        from models.rectangle import Rectangle
+        from models.square import Square
+        if list_objs is not None:
+            if cls is Rectangle:
+                list_objs = [[x.id, x.width, x.height, x.x, x.y]
+                             for x in list_objs]
+            else:
+                list_objs = [[x.id, x.size, x.x, x.y]
+                             for x in list_objs]
+        with open('{}.csv'.format(cls.__name__), 'w', newline='',
+                  encoding='utf-8') as f:
+            writer = csv.writer(f)
+            writer.writerows(list_objs)
+
+
+    @classmethod
+    def load_from_file_csv(cls):
+        '''Loads object to csv file.'''
+        from models.rectangle import Rectangle
+        from models.square import Square
+        ret_urn = []
+        with open('{}.csv'.format(cls.__name__), 'r', newline='',
+                  encoding='utf-8') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                row = [int(r) for r in row]
+                if cls is Rectangle:
+                    d = {"id": row[0], "width": row[1], "height": row[2],
+                         "x": row[3], "y": row[4]}
+                else:
+                    d = {"id": row[0], "size": row[1],
+                         "x": row[2], "y": row[3]}
+                ret_urn.append(cls.create(**d))
+        return ret_urn
