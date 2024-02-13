ff1f6638a01967795f4d170dfc91f303d99e74f5
HadeerAdelouf <hadiradel999@gmail.com>
2024-02-13 04:10:07 +0200

    task

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-
-if (process.argv[3]) {
-    console.log('Arguments found');
-  } else if (process.argv[2]) {
-    console.log('Argument found');
-  } else {
-    console.log('No argument');
-  }
+const { argv } = require('process');
+if (argv.length === 2) { console.log('No argument'); } else if (argv.length === 3) { console.log('Argument found'); } else { console.log('Arguments found'); }
