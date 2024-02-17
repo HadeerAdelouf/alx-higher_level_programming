0980bbf5131caefc4772596fe00e0c54ea865b87
HadeerAdelouf <hadiradel999@gmail.com>
2024-02-17 22:11:31 +0200

    fiix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,16 +4,22 @@ Script that takes in an argument and displays all values in the states
 table of hbtn_0e_0_usa where name matches the argument
 """
 
-import MySQLdb
+import MySQLdb as db
 from sys import argv
 
 if __name__ == '__main__':
-    db_connect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                                 passwd=sys.argv[2], db=sys.argv[3])
+    """
+    Access to the database and get the states
+    from the database
+    """
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
     db_cursor = db_connect.cursor()
+
     db_cursor.execute(
         "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
                         states.id ASC".format(argv[4]))
     rows = db_cursor.fetchall()
+
     for i in rows:
         print(i)
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 """
-Script that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument
+Script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument
+but safe from MySQL injections!
 """
 
-import MySQLdb
+import MySQLdb as db
 from sys import argv
 
-if __name__ == '__main__':
-    db_connect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                                 passwd=sys.argv[2], db=sys.argv[3])
+if __name__ == "__main__":
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+
     db_cursor = db_connect.cursor()
     db_cursor.execute(
         "SELECT * FROM states WHERE name LIKE \
                     BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]})
 
     rows = db_cursor.fetchall()
+
     for i in rows:
         print(i)
-
-    db_cursor.close()
-    db_connect.close()
