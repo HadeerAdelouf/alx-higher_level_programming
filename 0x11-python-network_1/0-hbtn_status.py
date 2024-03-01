65545b180f11704886dea20b3baac446a272d7e2
HadeerAdelouf <hadiradel999@gmail.com>
2024-03-02 01:54:11 +0200

    task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ import urllib.request
 
 if __name__ == "__main__":
     request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response :
+    with urllib.request.urlopen(request) as response:
         content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
